76074235bfc198baa6cb059db4bb41115c50d74a
Diego Matesanz <diegomatesanzfuente@gmail.com>
2025-01-26 20:49:00 +0100

    Added top app bars

--- app/src/main/java/com/cristiangoncas/snackmovement/ui/home/HomeScreen.kt
+++ app/src/main/java/com/cristiangoncas/snackmovement/ui/home/HomeScreen.kt
@@ -4,17 +4,23 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cristiangoncas.snackmovement.R
 import com.cristiangoncas.snackmovement.ui.Screen
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     viewModel: HomeViewModel = viewModel(),
@@ -23,7 +29,16 @@ fun HomeScreen(
 
     Screen {
         Scaffold(
-            topBar = { },
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text(text = stringResource(R.string.title_home))
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                    ),
+                )
+            },
         ) { padding ->
             Box(
                 modifier = Modifier
